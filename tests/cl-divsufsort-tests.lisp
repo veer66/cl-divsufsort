d234c746ddf5c0b1592b2773af17b812e018f58e
Vee Satayamas <veerpub@pm.me>
2026-02-14 22:32:24 +0700

    Improve tests

--- tests/cl-divsufsort-tests.lisp
+++ tests/cl-divsufsort-tests.lisp
@@ -4,8 +4,16 @@
 
 (in-suite :int-lg-suite)
 
-(test test-kaka
-  (loop for n in '(1 7)
+(test int-lg-test
+  (loop for n in
+	      (list 0 1 2 3 4 7 8 15 16 31 32 63 64 127 128
+		    255 256 511 512 1023 1024
+		    (1- (expt 2 32)) (expt 2 32) (1+ (expt 2 32))
+		    (1- (expt 2 48)) (expt 2 48) (1+ (expt 2 48))
+		    (1- (expt 2 56)) (expt 2 56) (1+ (expt 2 56))
+		    most-positive-fixnum)
 	do
-	   (is (= (cl-divsufsort:int-lg n)
-		  (ceiling (log n))))))
+	   (is (= (cl-divsufsort::int-lg n)
+		  (if (= n 0)
+		      -1
+		      (floor (log n 2)))))))
