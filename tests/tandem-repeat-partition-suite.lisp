4fdb23462c98cc9dbf7b05cb80dc6bbfd13b503b
Vee Satayamas <veerpub@pm.me>
2026-02-15 22:23:46 +0700

    Untabify

--- tests/tandem-repeat-partition-suite.lisp
+++ tests/tandem-repeat-partition-suite.lisp
@@ -4,9 +4,9 @@
 
 (test tandem-repeat-partition-basic
   (let ((rank (make-array 10 :element-type 'fixnum :initial-contents '(1 1 1 2 2 2 3 3 3 0)))
-	(arr (make-array 10 :element-type 'fixnum :initial-contents '(0 1 2 3 4 5 6 7 8 9)))
-	(pa (make-array 1 :element-type 'fixnum))
-	(pb (make-array 1 :element-type 'fixnum)))
+        (arr (make-array 10 :element-type 'fixnum :initial-contents '(0 1 2 3 4 5 6 7 8 9)))
+        (pa (make-array 1 :element-type 'fixnum))
+        (pb (make-array 1 :element-type 'fixnum)))
     (multiple-value-bind (a b) (cl-divsufsort::tandem-repeat-partition rank arr 0 5 10 pa pb 2)
       (is (= a (aref pa 0)))
       (is (= b (aref pb 0)))
@@ -15,9 +15,9 @@
 
 (test tandem-repeat-partition-all-equal
   (let ((rank (make-array 6 :element-type 'fixnum :initial-contents '(2 2 2 2 2 2)))
-	(arr (make-array 6 :element-type 'fixnum :initial-contents '(0 1 2 3 4 5)))
-	(pa (make-array 1 :element-type 'fixnum))
-	(pb (make-array 1 :element-type 'fixnum)))
+        (arr (make-array 6 :element-type 'fixnum :initial-contents '(0 1 2 3 4 5)))
+        (pa (make-array 1 :element-type 'fixnum))
+        (pb (make-array 1 :element-type 'fixnum)))
     (multiple-value-bind (a b) (cl-divsufsort::tandem-repeat-partition rank arr 0 3 6 pa pb 2)
       (is (= a (aref pa 0)))
       (is (= b (aref pb 0)))
@@ -26,9 +26,9 @@
 
 (test tandem-repeat-partition-nil-output
   (let ((rank (make-array 6 :element-type 'fixnum :initial-contents '(1 1 2 2 3 3)))
-	(arr (make-array 6 :element-type 'fixnum :initial-contents '(0 1 2 3 4 5)))
-	(pa (make-array 1 :element-type 'fixnum))
-	(pb (make-array 1 :element-type 'fixnum)))
+        (arr (make-array 6 :element-type 'fixnum :initial-contents '(0 1 2 3 4 5)))
+        (pa (make-array 1 :element-type 'fixnum))
+        (pb (make-array 1 :element-type 'fixnum)))
     (multiple-value-bind (a b) (cl-divsufsort::tandem-repeat-partition rank arr 0 2 6 pa pb 2)
       (is (= a (aref pa 0)))
       (is (= b (aref pb 0)))
@@ -37,7 +37,7 @@
 
 (test tandem-repeat-partition-no-pa-pb
   (let ((rank (make-array 6 :element-type 'fixnum :initial-contents '(1 1 2 2 3 3)))
-	(arr (make-array 6 :element-type 'fixnum :initial-contents '(0 1 2 3 4 5))))
+        (arr (make-array 6 :element-type 'fixnum :initial-contents '(0 1 2 3 4 5))))
     (multiple-value-bind (a b) (cl-divsufsort::tandem-repeat-partition rank arr 0 2 6 nil nil 2)
       (is (= a 2))
       (is (= b 4)))))
